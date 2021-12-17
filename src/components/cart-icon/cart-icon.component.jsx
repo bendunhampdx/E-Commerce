e750acb5d9b1d0a6f219c324ac3b6bc07016dc33
bendunhampdx <bendunhampdx@gmail.com>
2021-12-17 09:20:37 -0800

    add cart icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import './cart-icon.styles.scss';
 
-const CArtIcon = () => (
+const CartIcon = () => (
     <div className='cart-icon'>
       <ShoppingIcon className='shopping-icon' />
       <span className='item-count'>0</span>
     </div>
 );
+
+export default CartIcon;
 
 
 
