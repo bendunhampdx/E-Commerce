c4250aca3efc3352dfa64733198ad044dd23d364
bendunhampdx <bendunhampdx@gmail.com>
2021-12-17 13:21:39 -0800

    add state to cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -2,7 +2,8 @@ import CartIcon from '../../components/cart-icon/cart-icon.component'
 import CartActionTypes from './cart.types'
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,7 +12,19 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         hidden: !state.hidden
-      }
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload]
+      };
+    // case CartActionTypes.CLEAR_ITEM_FROM_CART:
+    //   return {
+    //     ...state,
+    //     cartItems: state.cartItems.filter(
+    //       cartItem => cartItem.id !== action.payload.id
+    //     )
+    //   };
     default:
       return state
   }
